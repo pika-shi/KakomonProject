5e9d9277c0ceecdbe821ec8f4876ad00b367db73
竹村 光 <takemura@dl.kuis.kyoto-u.ac.jp>
2012-09-22 19:05:59 +0900

    講義ページ完成

--- kuis-kakomon/kakomon/forms.py
+++ kuis-kakomon/kakomon/forms.py
@@ -3,8 +3,8 @@
 from kay.utils import forms
 
 class UploadForm(forms.Form):
-      year = forms.ChoiceField(choices=[2010, 2011, 2012], required=True)
+      year = forms.ChoiceField(choices=[2008, 2009, 2010, 2011, 2012, 2013, 2014], required=True)
       file = forms.FileField(required=True)
 
 class PasswordForm(forms.Form):
-      password = forms.TextField(required=True, widget=forms.PasswordInput)
+      password = forms.TextField(required=True, label='パスワード'.decode('utf-8'), widget=forms.PasswordInput)
--- kuis-kakomon/kakomon/views.py
+++ kuis-kakomon/kakomon/views.py
@@ -32,6 +32,15 @@ from kakomon.models import Lecture, Kakomon
 from kakomon.forms import (UploadForm, PasswordForm)
 
 def index(request):
+  '''
+  Lecture(id=1,name='A',grade=2,semester=1,comment='hogehoge').put()
+  Lecture(id=2,name='B',grade=2,semester=1,comment='hogehoge').put()
+  Lecture(id=3,name='C',grade=2,semester=1,comment='hogehoge').put()
+  Lecture(id=4,name='D',grade=2,semester=2,comment='hogehoge').put()
+  Lecture(id=5,name='E',grade=2,semester=2,comment='hogehoge').put()
+  Lecture(id=6,name='F',grade=3,semester=1,comment='hogehoge').put()
+  Lecture(id=7,name='G',grade=3,semester=1,comment='hogehoge').put()
+  '''
   return render_to_response('kakomon/index.html')
 
 def lectures(request, grade):
@@ -59,12 +68,15 @@ def authorize(request):
                              'method': request.method})
 
 def manage(request):
-  lectures = Lecture.all()
+  lectures = []
+  for i in range(1, 5):
+    lectures.append(Lecture.all().filter('grade =', i))
   return render_to_response('kakomon/manage.html',
                             {'lectures': lectures})
 
 def manage_lectures(request, id):
   form = UploadForm()
+  lecture = Lecture.all().filter('id =', id).get()
   if request.method == "POST" and form.validate(request.form, request.files):
     lecture = Lecture.all().filter('id =', id).get()
     mimetype = request.files['file'].content_type
@@ -72,4 +84,5 @@ def manage_lectures(request, id):
     Kakomon(lecture=lecture, year=form['year'],
             file=form['file'], mimetype=mimetype, ext=ext).put()
   return render_to_response('kakomon/manage_lecture.html',
-                            {'form': form.as_widget()})
+                            {'form': form.as_widget(),
+                             'lecture': lecture})
